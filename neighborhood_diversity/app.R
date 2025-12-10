5d69d79afc09b1e530897f9b342e250a5b00216c
cstacy2005 <cstacy1@macalester.edu>
2025-12-09 21:57:34 -0600

    neighborhood diversity app

--- neighborhood_diversity/app.R
+++ neighborhood_diversity/app.R
@@ -15,12 +15,11 @@ library(plotly)
 data_by_dist <- read_rds("data/diverse_data_by_dist.rds")
 data_by_year <- read_csv("data/diverse_data_by_year.csv")
 
-
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Neighborhood Diversity"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
@@ -38,7 +37,8 @@ ui <- fluidPage(
         # Show a plot of the generated distribution
         mainPanel(
           plotlyOutput("scatter"),
-          plotlyOutput("map")
+          plotlyOutput("map"),
+          plotlyOutput("bar")
         )
     )
 )
@@ -50,7 +50,8 @@ server <- function(input, output) {
       filter(metro_name == input$city) %>%
       ggplot(aes(x = entropy, y = distmiles)) +
       geom_point(aes(key = tract_id)) +
-      geom_smooth(span = input$span, se = FALSE, method = "loess")
+      geom_smooth(span = input$span, se = FALSE, method = "loess") +
+      theme_classic()
 
     ggplotly(p, source = "plotly_scatterplot") %>%
       event_register("plotly_selected")
@@ -60,15 +61,24 @@ server <- function(input, output) {
     p <- data_by_dist %>%
       filter(metro_name == input$city) %>%
       ggplot() +
-      geom_sf(aes(fill = entropy))
+      geom_sf(aes(fill = entropy)) +
+      theme_classic()
   }
   )
 
   output$bar <- renderPlotly({
     p <- data_by_dist %>%
+      pivot_longer(
+        cols = c("aian", "asian", "black", "hispanic", "two_or_more", "white"),
+        names_to = "race",
+        values_to = "count"
+      ) %>%
       filter(metro_name == input$city) %>%
-      ggplot() +
-      geom_sf(aes(fill = entropy))
+      group_by(race) %>%
+      summarize(num = sum(count)) %>%
+      ggplot(aes(x = race, y = num, fill = race)) +
+      geom_col() +
+      theme_classic()
   }
   )
 }
